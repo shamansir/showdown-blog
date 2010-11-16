1ac1a02b9a46e9bfdb3ce58e999eb77f4c15c24e
Anthony Kotenko (shaman.sir) <akotenko@iparkcorp.com>
2010-11-16 12:47:47 +0200

    editor + load by tags

--- js/swblog.js
+++ js/swblog.js
@@ -1,6 +1,7 @@
 var _sdconv = new Showdown.converter();
 var _sw = {};
 _sw.options = null;
+_sw.tagsLevels = {1:'rare',2:'repeated',3:'recent',5:'frequent',7:'common',10:'massive'};
 
 $(document).ready(function(){
 
@@ -25,6 +26,25 @@ $(document).ready(function(){
     }
     
 });
+
+_sw.notify = function(text) {
+    if (window.console && console) console.log(text);
+}
+
+_sw.log = function(text) {
+    if (window.console && console) console.log(text);
+}
+
+_sw.getXml = function(_url, _success, _complete, _error) {
+    $.ajax({
+        type: "GET",
+	    url: _url,
+	    dataType: "xml",
+	    success: _success,
+	    complete: _complete,
+	    error: _error
+    });
+}
 
 _sw.renderPost = function(postId, target, xml) {
     target.attr('id', 'sw-post-' + postId).addClass('sw-post');
@@ -71,66 +91,24 @@ _sw.applyOptions = function() {
 }
 
 _sw.loadOptions = function() {
-    $.ajax({
-        type: "GET",
-	    url: "./prefs.xml",
-	    dataType: "xml",
-	    success: function(xml) {
-	        _sw.options = {};
-            _sw.options.title = $(xml).find('title').text();
-            _sw.options.description = $(xml).find('description').text();
-            _sw.options.timeFormat = $(xml).find('date-format').text();            
-            _sw.options.dateFormat = $(xml).find('time-format').text();
-            _sw.options.postedAt = $(xml).find('posted-at').text();
-            _sw.options.permalinkPrefix = $(xml).find('permalink-prefix').text();
-            _sw.options.anchorPrefix = $(xml).find('anchor-prefix').text();
-            _sw.options.showPostsList = $(xml).find('show-post-lists').text().match(/^true$/);
-            _sw.options.showTagsCloud = $(xml).find('show-tags-cloud').text().match(/^true$/);
-	    },
-	    complete: function() {
-	        if (_sw.options) _sw.applyOptions();
-	    }
-    });
-}
-
-_sw.loadPost = function(postId) {
-    if (!postId.match(/^[\w\d-]+/)) {
-        alert('post ID \'' + postId + '\' is not ok');
-    }
-
-    $.ajax({
-        type: "GET",
-        url: "./posts/" + postId + ".xml",
-        dataType: "xml",
-        success: function(xml) {
-           var target = $('<div>');
-           $('body #posts').append(target);
-           _sw.renderPost(postId, target, $(xml));
-       }
-    });
-}
-
-_sw.loadByTags = function(tagsCode) {
-    if (!tagsCode.match(/^\*[\w\d,-]+/)) {
-        alert('tags code \'' + tagsCode + '\' is not ok');
-    }
-
-}
-
-_sw.loadAllPosts = function() {
-    $.ajax({
-        type: "GET",
-	    url: "./posts.xml",
-	    dataType: "xml",
-	    success: function(xml) {
-            $(xml).find('posts').find('post').each(function() {
-                _sw.loadPost($(this).text());
-            });
-	    },
-	    error: function(req, text, error) {
-	        alert('posts.xml is not found');
-	    }
-    }); 
+    _sw.getXml("./prefs.xml", 
+               function(xml) { // success
+	                _sw.options = {};
+                    _sw.options.title = $(xml).find('title').text();
+                    _sw.options.description = $(xml).find('description').text();
+                    _sw.options.timeFormat = $(xml).find('date-format').text();            
+                    _sw.options.dateFormat = $(xml).find('time-format').text();
+                    _sw.options.postedAt = $(xml).find('posted-at').text();
+                    _sw.options.permalinkPrefix = $(xml).find('permalink-prefix').text();
+                    _sw.options.anchorPrefix = $(xml).find('anchor-prefix').text();
+                    _sw.options.showPostsList = $(xml).find('show-post-lists').text().match(/^true$/);
+                    _sw.options.showTagsCloud = $(xml).find('show-tags-cloud').text().match(/^true$/);
+                    var tLevels = $(xml).find('tags-levels').text();
+                    if ((tLevels.length > 0) && tLevels.match(/^[\w\d\s\{\}\:\-\']+$/)) _sw.tagsLevels = eval(tLevels);
+	           },
+	           function() { // complete
+        	        if (_sw.options) _sw.applyOptions();
+	           });
 }
 
 _sw.checkMobile = function() {
@@ -143,28 +121,175 @@ _sw.checkMobile = function() {
     }
 }
 
+_sw.loadAllPosts = function() {
+    _sw.getXml("./posts.xml",
+               function(xml) { // success
+                   $(xml).find('posts').find('post').each(function() {
+                       _sw.loadPost($(this).text());
+                   });
+               }, 
+               null, // complete
+               function(req, text, error) { // error
+	               _sw.notify('posts.xml is not found');
+	           });
+}
+
+_sw.loadPost = function(postId) {
+    if (!postId.match(/^[\w\d-]+/)) {
+        _sw.notify('post ID \'' + postId + '\' is not ok');
+    }
+
+    _sw.getXml("./posts/" + postId + ".xml",
+               function(xml) { // success
+                   var target = $('<div>');
+                   $('body #posts').append(target);
+                   _sw.renderPost(postId, target, $(xml));
+               },
+               null, // complete
+               function() { // error
+                  _sw.notify('failed to load post \'' + postId + '\'');
+               });
+}
+
+_sw.loadByTags = function(tagsCode) {
+    if (!tagsCode.match(/^\*[\w\d,-]+/)) {
+        _sw.notify('tags code \'' + tagsCode + '\' is not ok');
+    }
+    
+    _sw.getXml("./posts.xml",
+               function(xml) { // success
+                    var tagsToLoad = tagsCode.substring(1).split(',');
+                    $(xml).find('posts').find('post').each(function() {
+                        _sw.loadPostIfHasTags($(this).text(), tagsToLoad);
+                    });
+               }, 
+               null, // complete
+               function(req, text, error) { // error
+	               _sw.notify('posts.xml is not found');
+	           });    
+    
+}
+
+_sw.addPostLink = function(target, postId) {
+    if (!postId.match(/^[\w\d-]+/)) {
+        _sw.notify('post ID \'' + postId + '\' is not ok');
+    }
+
+    _sw.getXml("./posts/" + postId + ".xml",
+               function(xml) { // success
+                   var postTitle = $(xml).find('post').find('title').text();
+                   var postlink = $('<a>').attr('href', '#' + postId)
+                                          .attr('title', (postTitle.length > 0) ? postTitle : postId)
+                                          .text((postTitle.length > 0) ? postTitle : postId)
+                                          .addClass('sw-posts-list-link');
+                   target.append($('<li>').append(postlink));
+               });
+}
+
 _sw.loadPostsList = function() {
     if (!_sw.options || (_sw.options && _sw.options.showPostsList)) {
-        $.ajax({
-            type: "GET",
-	        url: "./posts.xml",
-	        dataType: "xml",
-	        success: function(xml) {
-	            var i = 0;
-                $(xml).find('posts').find('post').each(function() {
-                    // TODO:
-                });
-	        },
-	        error: function(req, text, error) {
-	            alert('posts.xml is not found');
-	        }
-        });     
+        _sw.getXml("./posts.xml",
+                   function(xml) { // success
+                       var target = $('<ul>');
+                       $('#posts-list').append(target).addClass('sw-posts-list');                   
+                       $(xml).find('posts').find('post').each(function() {
+                           _sw.addPostLink(target, $(this).text());
+                       });
+                   }, 
+                   null, // complete
+                   function(req, text, error) { // error
+	                   _sw.notify('posts.xml is not found');
+	               });
+	} else $('#posts-list').hide();
+}
+
+_sw.loadPostIfHasTags = function(postId, tags) {
+    if (!postId.match(/^[\w\d-]+/)) {
+        _sw.notify('post ID \'' + postId + '\' is not ok');
     }
+
+    _sw.getXml("./posts/" + postId + ".xml",
+               function(xml) { // success
+                   var postTags = $(xml).find('tags').text().split(',');
+                   for (var i = 0; i < tags.length; i++) {
+                      for (var j = 0; j < postTags.length; j++) {
+                        if ((tags[i] == postTags[j]) && (tags[i].length > 0)) {
+                           var target = $('<div>');
+                           $('body #posts').append(target);
+                           _sw.renderPost(postId, target, $(xml));
+                           return;
+                        };
+                      }
+                   }
+               },
+               null, // complete
+               function() { // error
+                  _sw.notify('failed to load post \'' + postId + '\'');
+               });
+}
+
+_sw.pushTagsStats = function(allTags, postId, callback) {
+    if (!postId.match(/^[\w\d-]+/)) {
+        _sw.notify('post ID \'' + postId + '\' is not ok');
+    }
+
+    _sw.getXml("./posts/" + postId + ".xml",
+               function(xml) { // success
+                   var postTags = $(xml).find('post').find('tags').text().split(',');
+                   for (var i = 0; i < postTags.length; i++) {
+                       var tag = postTags[i];
+                       if (tag.length > 0) {
+                           if (allTags[tag]) allTags[tag]++;
+                           else allTags[tag] = 1;
+                       }
+                   }
+                   callback();
+               },
+               null, // complete
+               function() { // error
+                  _sw.notify('failed to load post \'' + postId + '\'');
+               });
 }
 
 _sw.loadTagsCloud = function() {
     if (!_sw.options || (_sw.options && _sw.options.showTagsCloud)) {
-        
+        _sw.getXml("./posts.xml",
+                   function(xml) { // success
+                       var tags = {}; // { 'a': 5, 'b': 6 }
+                       var target = $('<ul>');                       
+                       $('#tagcloud').append(target).addClass('sw-tagcloud');
+                       var posts = $(xml).find('posts').find('post');
+                       var gotPosts = 0;
+                       var callback = function() {
+                           gotPosts++;
+                           if (gotPosts == posts.length) { // got all posts                          
+                               for (var tagname in tags) { // add tags links
+                                   var taglink = $('<a>').attr('href', '?*' + tagname)
+                                                         .attr('title', tagname)
+                                                         .text(tagname)
+                                                         .addClass('sw-tagcloud-tag')
+                                                         .addClass('sw-tagcloud-tag-count-' + tags[tagname])
+                                                         .addClass('sw-tagcloud-tag-level-' + _sw.tagLevelByCount(tags[tagname]));
+                                   target.append($('<li>').append(taglink));
+                               }
+                           }
+                       }
+                       posts.each(function() {
+                            _sw.pushTagsStats(tags, $(this).text(), callback);
+                       });
+                   }, 
+                   null, // complete
+                   function(req, text, error) { // error
+	                   _sw.notify('posts.xml is not found');
+	               });
+	} else $('#tagcloud').hide();
+}
+
+_sw.tagLevelByCount = function(count) {
+    if (!_sw.tagsLevels) return 'undefined'; 
+    for (var level in _sw.tagsLevels) {
+        if (count >= level) return _sw.tagsLevels[level]; 
     }
+    return 'default';
 }
 
