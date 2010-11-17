c3f7663e7ac96644c250308e2b12aa2706ab5068
Anthony Kotenko (shaman.sir) <akotenko@iparkcorp.com>
2010-11-17 13:57:40 +0200

    - test.rss

--- pack.sh
+++ pack.sh
@@ -1,2 +1,3 @@
+rm -Rf ./swblog.zip
 find ./ -name '*~' -exec rm '{}' \; -print -or -name ".*~" -exec rm {} \; -print
 zip -r ./swblog.zip ./index.html ./prefs.xml ./posts.xml ./js ./css ./_docs ./_licenses ./posts ./create.html ./makerss.sh ./rss.xsl
