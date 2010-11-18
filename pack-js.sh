996ba1124c99d13a2fa233d47b50652bbe01ec08
Ulric Wilfred <shaman.sir@gmail.com>
2010-11-18 21:24:14 +0200

    improved js-version packer

---
+++ pack-js.sh
@@ -0,0 +1,19 @@
+rm -Rf ./swblog-js.zip
+find ./ -name '*~' -exec rm '{}' \; -print -or -name ".*~" -exec rm {} \; -print
+zip -r \
+./swblog-js.zip \
+./index.html \
+./prefs.xml \
+./posts.xml \
+./js/fdate.min.js \
+./js/jquery-1.4.3.min.js \
+./js/showdown.min.js \
+./js/showdown-gui.js \
+./js/swblog.min.js \
+./css \
+./_docs \
+./_licenses \
+./posts \
+./create.html \
+./makerss.sh \
+./rss.xsl
